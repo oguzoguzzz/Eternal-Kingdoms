d1649074c6488d790d76efe2b08c667ad336315f
Oğuz Kaan Oğuz <oguzhan746@hotmail.com>
2024-09-06 22:53:20 +0300

    ss
    ss

--- Client/Assets/Scripts/CameraController.cs
+++ Client/Assets/Scripts/CameraController.cs
@@ -6,14 +6,15 @@ namespace DevelopersHub.RealtimeNetworking
 
     public class CameraController : MonoBehaviour
     {
-        private static CameraController _instance = null; public static CameraController instance { get { return _instance;}}
+
+        private static CameraController _instance = null; public static CameraController instance { get { return _instance; } }
+
         [SerializeField] private Camera _camera = null;
         [SerializeField] private float _moveSpeed = 50;
         [SerializeField] private float _moveSmooth = 5;
 
         [SerializeField] private float _zoomSpeed = 5f;
         [SerializeField] private float _zoomSmooth = 5;
-
 
         private Controls _inputs = null;
 
@@ -33,14 +34,14 @@ namespace DevelopersHub.RealtimeNetworking
         private float _zoomBaseValue = 0;
         private float _zoomBaseDistance = 0;
 
-
         private Transform _root = null;
         private Transform _pivot = null;
         private Transform _target = null;
 
-        private bool _building = false; public bool isPlacingBuilding { get{ return _building;} set {_building = value;}}
+        private bool _building = false; public bool isPlacingBuilding { get { return _building; } set { _building = value; } }
         private Vector3 _buildBasePosition = Vector3.zero;
         private bool _movingBuilding = false;
+
         private void Awake()
         {
             _instance = this;
@@ -51,11 +52,13 @@ namespace DevelopersHub.RealtimeNetworking
             _camera.orthographic = true;
             _camera.nearClipPlane = 0;
         }
+
         private void Start()
         {
-            Initialize(Vector3.zero, 40 , 40, 40, 40, 45, 10, 5, 20);
+            Initialize(Vector3.zero, 40, 40, 40, 40, 45, 10, 5, 20);
         }
-        private void Initialize(Vector3 center, float right, float left, float up, float down, float angle, float zoom, float zoomMin, float zoomMax)
+
+        public void Initialize(Vector3 center, float right, float left, float up, float down, float angle, float zoom, float zoomMin, float zoomMax)
         {
             _center = center;
             _right = right;
@@ -75,7 +78,7 @@ namespace DevelopersHub.RealtimeNetworking
             _target.SetParent(_pivot);
 
             _root.position = _center;
-            _root.localEulerAngles = Vector3. zero;
+            _root.localEulerAngles = Vector3.zero;
 
             _pivot.localPosition = Vector3.zero;
             _pivot.localEulerAngles = new Vector3(_angle, 0, 0);
@@ -83,24 +86,25 @@ namespace DevelopersHub.RealtimeNetworking
             _target.localPosition = new Vector3(0, 0, -100);
             _target.localEulerAngles = Vector3.zero;
         }
+
         private void OnEnable()
         {
             _inputs.Enable();
             _inputs.Main.Move.started += _ => MoveStarted();
             _inputs.Main.Move.canceled += _ => MoveCanceled();
-
             _inputs.Main.TouchZoom.started += _ => ZoomStarted();
             _inputs.Main.TouchZoom.canceled += _ => ZoomCanceled();
         }
+
         private void OnDisable()
         {
             _inputs.Main.Move.started -= _ => MoveStarted();
             _inputs.Main.Move.canceled -= _ => MoveCanceled();
-
             _inputs.Main.TouchZoom.started -= _ => ZoomStarted();
             _inputs.Main.TouchZoom.canceled -= _ => ZoomCanceled();
             _inputs.Disable();
         }
+
         private void MoveStarted()
         {
             if (UI_Main.instance.isActive)
@@ -114,44 +118,52 @@ namespace DevelopersHub.RealtimeNetworking
                         _movingBuilding = true;
                     }
                 }
-                if (_movingBuilding == false)
+
+                if(_movingBuilding == false)
                 {
                     _moving = true;
                 }
             }
         }
+
         private void MoveCanceled()
         {
             _moving = false;
             _movingBuilding = false;
         }
+
         private void ZoomStarted()
         {
-            Vector2 touch0 = _inputs.Main.TouchPosition0.ReadValue<Vector2>();
-            Vector2 touch1 = _inputs.Main.TouchPosition1.ReadValue<Vector2>();
-            _zoomPositionOnScreen = Vector2.Lerp(touch0, touch1, 0.5f);
-            _zoomPositionInWorld = CameraScreenPositionToPlanePosition(_zoomPositionOnScreen);
-            _zoomBaseValue = _zoom;
+            if (UI_Main.instance.isActive)
+            {
+                Vector2 touch0 = _inputs.Main.TouchPosition0.ReadValue<Vector2>();
+                Vector2 touch1 = _inputs.Main.TouchPosition1.ReadValue<Vector2>();
+                _zoomPositionOnScreen = Vector2.Lerp(touch0, touch1, 0.5f);
+                _zoomPositionInWorld = CameraScreenPositionToPlanePosition(_zoomPositionOnScreen);
+                _zoomBaseValue = _zoom;
 
-            touch0.x /= Screen.width;
-            touch1.x /= Screen.width;
-            touch0.y /= Screen.height;
-            touch1.y /= Screen.height;
+                touch0.x /= Screen.width;
+                touch1.x /= Screen.width;
+                touch0.y /= Screen.height;
+                touch1.y /= Screen.height;
 
-            _zoomBaseDistance = Vector2.Distance(touch0, touch1);
-
-            _zooming = true;
+                _zoomBaseDistance = Vector2.Distance(touch0, touch1);
+                _zooming = true;
+            }
         }
+
         private void ZoomCanceled()
         {
             _zooming = false;
         }
-        private void Update ()
+
+        private void Update()
         {
+
             if (Input.touchSupported == false)
             {
                 float mouseScroll = _inputs.Main.MouseScroll.ReadValue<float>();
-                if (mouseScroll > 0)
+                if(mouseScroll > 0)
                 {
                     _zoom -= 3f * Time.deltaTime;
                 }
@@ -161,7 +173,7 @@ namespace DevelopersHub.RealtimeNetworking
                 }
             }
 
-            if ( _zooming)
+            if (_zooming)
             {
                 Vector2 touch0 = _inputs.Main.TouchPosition0.ReadValue<Vector2>();
                 Vector2 touch1 = _inputs.Main.TouchPosition1.ReadValue<Vector2>();
@@ -181,7 +193,7 @@ namespace DevelopersHub.RealtimeNetworking
             else if (_moving)
             {
                 Vector2 move = _inputs.Main.MoveDelta.ReadValue<Vector2>();
-                if (move != Vector2.zero)
+                if(move != Vector2.zero)
                 {
                     move.x /= Screen.width;
                     move.y /= Screen.height;
@@ -196,16 +208,15 @@ namespace DevelopersHub.RealtimeNetworking
             {
                 _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _zoom, _zoomSmooth * Time.deltaTime);
             }
-
             if (_camera.transform.position != _target.position)
             {
                 _camera.transform.position = Vector3.Lerp(_camera.transform.position, _target.position, _moveSmooth * Time.deltaTime);
             }
-
             if (_camera.transform.rotation != _target.rotation)
             {
                 _camera.transform.rotation = _target.rotation;
             }
+
             if (_building && _movingBuilding)
             {
                 Vector3 pos = CameraScreenPositionToPlanePosition(_inputs.Main.PointerPosition.ReadValue<Vector2>());
@@ -215,11 +226,11 @@ namespace DevelopersHub.RealtimeNetworking
 
         private void AdjustBounds()
         {
-            if (_zoom < _zoomMin)
+            if(_zoom < _zoomMin)
             {
                 _zoom = _zoomMin;
             }
-            if (_zoom > _zoomMax)
+            if(_zoom > _zoomMax)
             {
                 _zoom = _zoomMax;
             }
@@ -227,13 +238,13 @@ namespace DevelopersHub.RealtimeNetworking
             float h = PlaneOrtographicSize();
             float w = h * _camera.aspect;
 
-            if (h > (_up + _down) / 2f)
+            if(h > (_up + _down) / 2f)
             {
                 float n = (_up + _down) / 2f;
                 _zoom = n * Mathf.Sin(_angle * Mathf.Deg2Rad);
             }
 
-            if ( w > (_right + _left) / 2f)
+            if (w > (_right + _left) / 2f)
             {
                 float n = (_right + _left) / 2f;
                 _zoom = n * Mathf.Sin(_angle * Mathf.Deg2Rad) / _camera.aspect;
@@ -245,9 +256,9 @@ namespace DevelopersHub.RealtimeNetworking
             Vector3 tr = _root.position + _root.right.normalized * w + _root.forward.normalized * h;
             Vector3 tl = _root.position - _root.right.normalized * w + _root.forward.normalized * h;
             Vector3 dr = _root.position + _root.right.normalized * w - _root.forward.normalized * h;
-            Vector3 dl = _root.position - _root.right.normalized * w + _root.forward.normalized * h;
+            Vector3 dl = _root.position - _root.right.normalized * w - _root.forward.normalized * h;
 
-            if (tr.x > _center.x + _right)
+            if(tr.x > _center.x + _right)
             {
                 _root.position += Vector3.left * Mathf.Abs(tr.x - (_center.x + _right));
             }
@@ -271,6 +282,7 @@ namespace DevelopersHub.RealtimeNetworking
             float h = _zoom * 2f;
             return h / Mathf.Sin(_angle * Mathf.Deg2Rad) / 2f;
         }
+
         private Vector3 CameraScreenPositionToWorldPosition(Vector2 position)
         {
             float h = _camera.orthographicSize * 2f;
@@ -278,12 +290,14 @@ namespace DevelopersHub.RealtimeNetworking
             Vector3 ancher = _camera.transform.position - (_camera.transform.right.normalized * w / 2f) - (_camera.transform.up.normalized * h / 2f);
             return ancher + (_camera.transform.right.normalized * position.x / Screen.width * w) + (_camera.transform.up.normalized * position.y / Screen.height * h);
         }
-        private Vector3 CameraScreenPositionToPlanePosition(Vector2 position)
+
+        public Vector3 CameraScreenPositionToPlanePosition(Vector2 position)
         {
             Vector3 point = CameraScreenPositionToWorldPosition(position);
             float h = point.y - _root.position.y;
             float x = h / Mathf.Sin(_angle * Mathf.Deg2Rad);
             return point + _camera.transform.forward.normalized * x;
         }
+
     }
 }
